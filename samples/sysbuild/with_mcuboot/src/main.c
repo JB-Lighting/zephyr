510f56b19e23ec5eb45d51b9dae798cc5654cb91
Thomas Decker <decker@jb-lighting.de>
2025-11-17 09:20:25 +0100

    arch: arm: mpu: Add arm_mpu_regions_print() for debugging
    Adds function arm_mpu_regions_print() when CONFIG_MPU_REGIONS_PRINT is
    set. Reads out the current MPU region configuration and prints it to
    console. Only usable for debugging SOC_STM32H7RS.
    
    Signed-off-by: Thomas Decker <decker@jb-lighting.de>

--- samples/sysbuild/with_mcuboot/src/main.c
+++ samples/sysbuild/with_mcuboot/src/main.c
@@ -7,9 +7,18 @@
 #include <zephyr/kernel.h>
 #include <zephyr/linker/linker-defs.h>
 
+#ifdef CONFIG_MPU_REGIONS_PRINT 
+#include <zephyr/arch/arm/mpu/arm_mpu_regions_print.h>
+#endif
+
 int main(void)
 {
 	printk("Address of sample %p\n", (void *)__rom_region_start);
 	printk("Hello sysbuild with mcuboot! %s\n", CONFIG_BOARD);
+
+#ifdef CONFIG_MPU_REGIONS_PRINT 
+	arm_mpu_regions_print();
+#endif
+
 	return 0;
 }
