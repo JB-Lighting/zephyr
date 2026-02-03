0e228094f8fa614096536fd5211430c4659540a5
Thomas Decker <decker@jb-lighting.de>
2026-02-03 13:39:56 +0100

    drivers: serial: uart_stm32: move device init to uart_stm32_pm_action
    Changes driver init behaviour: uart_stm32_init() delegates the init to
    uart_stm32_pm_action(). When CONFIG_PM_DEVICE is not set, this code is
    immediately called. With CONFIG_PM_DEVICE set, uart is initialized
    only when the device resumes.
    
    With this change, the gpios are only enabled (PINCTRL_STATE_DEFAULT) while
    the device is active. This helps manage party unpowered external
    peripherals with PM.
    
    Signed-off-by: Thomas Decker <decker@jb-lighting.de>

--- drivers/serial/uart_stm32.c
+++ drivers/serial/uart_stm32.c
@@ -2366,50 +2366,6 @@ static int uart_stm32_registers_configure(const struct device *dev)
 	return 0;
 }
 
-/**
- * @brief Initialize UART channel
- *
- * This routine is called to reset the chip in a quiescent state.
- * It is assumed that this function is called only once per UART.
- *
- * @param dev UART device struct
- *
- * @return 0
- */
-static int uart_stm32_init(const struct device *dev)
-{
-	const struct uart_stm32_config *config = dev->config;
-	int err;
-
-	err = uart_stm32_clocks_enable(dev);
-	if (err < 0) {
-		return err;
-	}
-
-	/* Configure dt provided device signals when available */
-	err = pinctrl_apply_state(config->pcfg, PINCTRL_STATE_DEFAULT);
-	if (err < 0) {
-		return err;
-	}
-
-	err = uart_stm32_registers_configure(dev);
-	if (err < 0) {
-		return err;
-	}
-
-#if defined(CONFIG_PM) || \
-	defined(CONFIG_UART_INTERRUPT_DRIVEN) || \
-	defined(CONFIG_UART_ASYNC_API)
-	config->irq_config_func(dev);
-#endif /* CONFIG_PM || CONFIG_UART_INTERRUPT_DRIVEN || CONFIG_UART_ASYNC_API */
-
-#ifdef CONFIG_UART_ASYNC_API
-	return uart_stm32_async_init(dev);
-#else
-	return 0;
-#endif
-}
-
 #ifdef CONFIG_PM_DEVICE
 static void uart_stm32_suspend_setup(const struct device *dev)
 {
@@ -2431,6 +2387,7 @@ static void uart_stm32_suspend_setup(const struct device *dev)
 	/* Clear OVERRUN flag */
 	LL_USART_ClearFlag_ORE(usart);
 }
+#endif /* CONFIG_PM_DEVICE */
 
 static int uart_stm32_pm_action(const struct device *dev, enum pm_device_action action)
 {
@@ -2445,14 +2402,15 @@ static int uart_stm32_pm_action(const struct device *dev, enum pm_device_action 
 			return err;
 		}
 
-		/* Enable bus clock */
-		err = clock_control_on(config->clock, (clock_control_subsys_t)&config->pclken[0]);
-		if (err < 0) {
-			LOG_ERR("Could not enable (LP)UART clock");
-			return err;
-		}
-
-		if (!LL_USART_IsEnabled(config->usart)) {
+		if (LL_USART_IsEnabled(config->usart)) {
+			/* Only re-enable bus clock (was disabled during suspend) */
+			err = clock_control_on(config->clock,
+					       (clock_control_subsys_t)&config->pclken[0]);
+			if (err < 0) {
+				LOG_ERR("Could not enable (LP)UART clock");
+				return err;
+			}
+		} else {
 			/* When exiting low power mode, check whether UART is enabled.
 			 * If not, it means the peripheral has been powered down
 			 * by the low-power mode. If suspend-to-RAM is enabled,
@@ -2464,17 +2422,30 @@ static int uart_stm32_pm_action(const struct device *dev, enum pm_device_action 
 			 *
 			 * STOP2 on STM32WLE5 is an example of such low-power mode.
 			 */
-			if (IS_ENABLED(CONFIG_PM_S2RAM)) {
-				err = uart_stm32_init(dev);
-			} else {
-				err = uart_stm32_registers_configure(dev);
-			}
 
+			err = uart_stm32_clocks_enable(dev);
 			if (err < 0) {
 				return err;
 			}
+
+			err = uart_stm32_registers_configure(dev);
+			if (err < 0) {
+				return err;
+			}
+
+#if defined(CONFIG_PM) || defined(CONFIG_UART_INTERRUPT_DRIVEN) || defined(CONFIG_UART_ASYNC_API)
+			config->irq_config_func(dev);
+#endif /* CONFIG_PM || CONFIG_UART_INTERRUPT_DRIVEN || CONFIG_UART_ASYNC_API */
+
+#ifdef CONFIG_UART_ASYNC_API
+			err = uart_stm32_async_init(dev);
+			if (err < 0) {
+				return err;
+			}
+#endif /* CONFIG_UART_ASYNC_API */
 		}
 		break;
+#ifdef CONFIG_PM_DEVICE
 	case PM_DEVICE_ACTION_SUSPEND:
 		uart_stm32_suspend_setup(dev);
 		/* Stop device clock. Note: fixed clocks are not handled yet. */
@@ -2497,13 +2468,40 @@ static int uart_stm32_pm_action(const struct device *dev, enum pm_device_action 
 			return err;
 		}
 		break;
+#endif /* CONFIG_PM_DEVICE */
 	default:
 		return -ENOTSUP;
 	}
 
 	return 0;
 }
-#endif /* CONFIG_PM_DEVICE */
+
+/**
+ * @brief Initialize UART channel
+ *
+ * This routine is called to reset the chip in a quiescent state.
+ * It is assumed that this function is called only once per UART.
+ *
+ * @param dev UART device struct
+ *
+ * @return 0
+ */
+static int uart_stm32_init(const struct device *dev)
+{
+	const struct uart_stm32_config *config = dev->config;
+
+	/* Make sure the first call to uart_stm32_pm_action does a full initialization, because the
+	 * uart could be left enabled by a bootloader.
+	 */
+	LL_USART_Disable(config->usart);
+
+	/* When CONFIG_PM_DEVICE is not set, the callback is directly
+	 * called with PM_DEVICE_ACTION_RESUME and the uart gets initialized
+	 * there. Otherwise PM DEVICE takes care of initializing and de-init.
+	 */
+
+	return pm_device_driver_init(dev, uart_stm32_pm_action);
+}
 
 #ifdef CONFIG_UART_ASYNC_API
 /* src_dev and dest_dev should be 'MEMORY' or 'PERIPHERAL'. */
